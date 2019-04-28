068b1f0121ff76da340cec782b699b625caaffe8
F1uder <flud-zlo@yandex.ru>
2019-04-28 13:45:38 +0300

    add lang

---
+++ en/navigation.php
@@ -0,0 +1,225 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'navbar.news'   => 'News',
+    'navbar.maps'   => 'Maps',
+    'navbar.fish'   => 'Fish',
+    'navbar.item'   => 'Items and prices',
+    'navbar.cont'   => 'Contacts',
+    'navbar.db'     => 'Knowledge base',
+    'navbar.lure'   => 'Groundbait',
+    'navbar.video'  => 'streams and videos',
+
+    'navbar.title'  => 'Navigation',
+
+    'navbar.registration'   => 'Register',
+    'navbar.options'        => 'Settings',
+    'navbar.logout'         => 'Logout',
+    'navbar.login'          => 'Login',
+
+    // breadcrumbs
+    'breadcrumbs.home'      => 'Home',
+    'breadcrumbs.allfish'   => 'List of all fish',
+    'breadcrumbs.items'     => 'Game items',
+    'breadcrumbs.maps'      => 'All Maps',
+
+    // sidebar
+    'sidebar.items.title'   => 'Game items and prices',
+    'sidebar.dev' => 'The site is under development, so not everything works as we would like.',
+    'sidebar.ofsite' => 'official site',
+    'sidebar.suppbtn' => '$$$ Donate',
+    'sidebar.dontext' => 'You can support me (the developer of this site) in the game (game nickname: Fluder) or donate.',
+    'sidebar.returnmap' => 'Go to map',
+
+    // categories
+    'categories.udilishcha'     => 'Rods',
+    'categories.katushki'       => 'Reels',
+    'categories.prikormka'      => 'Groundbait',
+
+
+    'categories.sub.makhovye'           => 'Telescopic rods',
+    'categories.sub.spinningovye'       => 'Spinning rods',
+    'categories.sub.fidery'             => 'Feeder rods',
+    'categories.sub.karpovye'           => 'Carp rods',
+    'categories.sub.matchevye'          => 'Match rods',
+    'categories.sub.dzherkovye'         => 'Jerking rods',
+
+    'categories.sub.bezynertsionnye'    => 'Spinning reels',
+
+    'categories.sub.osnovy'         => 'Basics',
+    'categories.sub.dobavki'        => 'Additives',
+    'categories.sub.aromatizatory'  => 'Attractants',
+
+    // items
+    'items.price'  => 'Price',
+    'items.price.min'  => 'Price [min]',
+    'items.price.max'  => 'Price [max]',
+    'items.price.from' => 'from',
+    'items.sort'       => 'Sorting',
+
+    'items.table.image' => 'Picture',
+    'items.table.name'  => 'Name + Parameters',
+    'items.table.desc'  => 'detailed information',
+    'items.table.button'  => 'Read more',
+    'items.table.close'  => 'Close',
+    'items.report' => 'Report a bug',
+
+    'items.stat.strength' => 'Load capacity',
+    'items.stat.length' => 'Length',
+    'items.stat.size' => 'Size',
+    'items.stat.brake' => 'Max drag',
+    'items.stat.type' => 'Type',
+    'items.stat.sens' => 'Sensitivity',
+    'items.stat.mode' => 'Action',
+    'items.stat.rigidity' => 'Stiffness',
+    'items.stat.power' => 'Power',
+    'items.stat.test' => 'Test',
+    'items.stat.weight' => 'Weight',
+    'items.stat.ban' => 'Restrictions',
+    'items.stat.bonus' => 'Bonus',
+    'items.stat.per' => 'Gear ratio',
+    'items.stat.skill' => 'Skill',
+
+    'items.stat.kg' => 'kg',
+    'items.stat.metr' => 'm',
+    'items.stat.gramm' => 'g',
+
+    // register
+    'reg.name' => 'Game nickname or name',
+    'reg.pass' => 'Password',
+    'reg.pass2' => 'Confirm password',
+    'reg.rules' => 'Rules',
+    'reg.btn' => 'Register',
+
+    // login
+    'login.email' => 'E-mail',
+    'login.pass' => 'Password',
+    'login.save' => 'Remember me',
+    'login.enter' => 'Login',
+    'login.title' => 'Login',
+
+    // support
+    'supportme' => 'The RuFish4 project is not commercial, you can support me financially, through the form below page.',
+    'suppbtn' => 'Donate',
+
+    // fish
+    'fishlist' => 'List of all fish and their habitats',
+    'fish_title' => 'Fish',
+    'fish_loc' => 'Habitat',
+    'fish_coor' => 'Habitat coordinates',
+    'fish_desc' => 'Here, only those coordinates that have been added to our site by our users.',
+
+    // lure
+    'lure_add' => 'Add your own recipe',
+    'lure_fish' => 'Groundbaits for fish',
+    'lure_collapse' => 'Expand or collapse the list',
+    'lure_rec' => 'Recipe',
+    'lure_notdata' => 'No data yet :(',
+    'lure.osnovy'         => 'Base',
+    'lure.dobavki'        => 'Additive',
+    'lure.aromatizatory'  => 'Attractant',
+    'lure.skill' => 'Skill level required',
+
+    'fish' => [
+        '1' => 'Grass Carp',
+        '2' => 'Black Carp',
+        '3' => 'White-eye Bream',
+        '4' => 'Volga Zander',
+        '5' => 'Chub',
+        '6' => 'Arctic Char',
+        '7' => 'Kuori Char',
+        '8' => 'White Bream',
+        '9' => 'Zebra Mussel',
+        '10' => 'Dace',
+        '11' => 'Ruffe',
+        '12' => 'Donets ruffe',
+        '13' => 'Asp',
+        '14' => 'Crucian Carp',
+        '15' => 'Gibel Carp',
+        '16' => 'Leather carp',
+        '17' => 'Mirror Carp',
+        '18' => 'Common Carp',
+        '19' => 'Rudd',
+        '20' => 'Bream',
+        '21' => 'Tench',
+        '22' => 'Golden Tench',
+        '23' => 'Atlantic Salmon',
+        '24' => 'Frog',
+        '25' => 'Burbot',
+        '26' => 'Perch',
+        '27' => 'Russian Sturgeon',
+        '28' => 'Char',
+        '29' => 'River mussel',
+        '30' => 'Gudgeon',
+        '31' => 'Common Roach',
+        '32' => 'Silver Bream',
+        '33' => 'Nase',
+        '34' => 'Freshwater Crayfish',
+        '35' => 'Chinese Sleeper',
+        '36' => 'Vimba',
+        '37' => 'Vendace',
+        '38' => 'Wild carp',
+        '39' => 'Blue Bream',
+        '40' => 'Catfish',
+        '41' => 'Sterlet',
+        '42' => 'Zander',
+        '43' => 'Taimen',
+        '44' => 'Silver carp',
+        '45' => 'Bighead carp',
+        '46' => 'Eel',
+        '47' => 'Bleak',
+        '48' => 'Common Barbel',
+        '49' => 'Lake Trout',
+        '50' => 'Brown trout',
+        '51' => 'Sevan trout',
+        '52' => 'Grayling',
+        '53' => 'Sichel',
+        '54' => 'Pike',
+        '55' => 'Ide',
+        '56' => 'Three-spined stickleback',
+        '57' => 'Smelt',
+        '58' => 'Common minnow',
+        '59' => 'Baltic sturgeon',
+        '60' => 'Gray char',
+        '61' => 'Red char',
+        '62' => 'Ripus',
+        '63' => 'Volkhov whitefish',
+        '64' => 'Valaam whitefish',
+        '65' => 'Vuoksa whitefish',
+        '66' => 'Ludoga whitefish',
+        '67' => 'Beloribitsa Whitefish',
+        '68' => 'Beluga',
+        '69' => 'Buffalo',
+        '70' => 'Black Buffalo',
+        '71' => 'Caspian Roach',
+        '72' => 'Loach',
+        '73' => 'Small Southern Stickleback',
+        '74' => 'Caspian Kutum',
+        '75' => 'Eastern Bream',
+        '76' => 'Caspian Brown Trout',
+        '77' => 'Caspian Lamprey',
+        '78' => 'Persian Sturgeon',
+        '79' => 'Black-spined Herring',
+        '80' => 'Stellate Sturgeon',
+        '81' => 'Brazhnikov Herring',
+        '82' => 'Kesslers Herring',
+        '83' => 'Albino Catfish',
+        '84' => 'Short-headed Barbel',
+        '85' => 'Shemaya',
+        '86' => 'Bastard Sturgeon',
+    ],
+
+
+];
