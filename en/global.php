e40df447de14f45147c93ec7f010b35aab3b45ef
f1uder <flud-zlo@yandex.ru>
2020-11-19 08:39:53 +0300

    update

--- en/global.php
+++ en/global.php
@@ -1,76 +1,76 @@
 <?php
 
 return [
-    'name' => 'Русская рыбалка',
-    'fun-site' => 'Фан-сайт',
-    'no-data' => 'Актуальных данных нет!',
-    'no-data-v2' => 'Нет данных',
-    'anonymous' => 'Аноним',
-    'support' => 'Тех-поддержка',
-    'section-dev' => 'Раздел находится в стадии разработки',
-    'coordinate' => 'координаты',
-    'more' => 'Подробнее',
-    'donate' => 'Поддержать сайт',
+    'name' => 'Russian fishing',
+    'fun-site' => 'Fan site',
+    'no-data' => 'No actual data!',
+    'no-data-v2' => 'There is no data',
+    'anonymous' => 'Anonymous',
+    'support' => 'Technical support',
+    'section-dev' => 'Section is under construction',
+    'coordinate' => 'coordinates',
+    'more' => 'More details',
+    'donate' => 'Donate',
 
     'navigation' => [
-        'news' => 'Новости',
-        'maps' => 'Водоемы и координаты',
-        'video' => 'Стримы и видео',
-        'wiki' => 'База знаний',
-        'contact' => 'Контакты',
-        'fish' => 'Список рыб',
-        'items' => 'Предметы и цены',
-        'weekly-records' => 'Недельные рекорды',
-        'more' => 'Смотреть подробнее',
+        'news' => 'News',
+        'maps' => 'Maps and coordinates',
+        'video' => 'Streams and videos',
+        'wiki' => 'Wiki',
+        'contact' => 'Contacts',
+        'fish' => 'List of fish',
+        'items' => 'Items and prices',
+        'weekly-records' => 'Weekly records',
+        'more' => 'See details',
     ],
 
     'home' => [
-        'new_video' => 'Новые видео по игре',
-        'new_point' => 'Новые координаты ловли',
-        'new_news' => 'Последние новости',
+        'new_video' => 'New videos on the game',
+        'new_point' => 'New fishing coordinates',
+        'new_news' => 'Last news',
     ],
 
     'tackle' => [
-        '1' => 'Поплавочная',
-        '2' => 'Донная',
-        '3' => 'Спиннинг',
+        '1' => 'Float rod',
+        '2' => 'Bottom fishing rod',
+        '3' => 'Spinning rod',
     ],
 
     'video' => [
-      'title' => 'Видео раздел по игре Русская рыбалка 4',
-      'user-video' => 'Видео от наших пользователей',
+      'title' => 'Video section on the game Russian Fishing 4',
+      'user-video' => 'Videos from our users',
     ],
 
     'map' => [
-        'all-coordinate' => 'Всего координат',
-        'required-level' => 'Требуемый уровень',
-        'show-map' => 'Показать карту',
+        'all-coordinate' => 'Total coordinates',
+        'required-level' => 'Required level',
+        'show-map' => 'Show map',
     ],
 
     'point' => [
-        'fish-list' => 'Здесь можно поймать следующие виды рыб',
-        'go-map' => 'Перейти к водоему',
-        'info' => 'Подробная информация',
+        'fish-list' => 'Here you can catch the following types of fish',
+        'go-map' => 'Go to the map',
+        'info' => 'Detailed information',
         'message' => [
             'line1' => 'Помните, что уловистым точкам нужно давать передышку. Если клев закончился, должно пройти немного времени, прежде чем на опустевшем месте вновь появится рыба.',
             'line2' => 'Также не стоит забывать про фазы, когда одна рыба сменяет другую на какой-то промежуток времени.',
             'line3' => 'Находясь на точке, можно ориентироваться по чату, что ловят в данный момент.',
         ],
-        'fish-view' => 'Рыбы, замеченные игроками',
+        'fish-view' => 'Fish spotted by players',
     ],
 
     'fish' => [
-        'title' => 'Список всех рыб и места их обитания',
-        'name' => 'Название',
-        'weight' => 'Вес',
-        'title-weight' => 'Минимальный - максимальный вес рыбы',
-        'where' => 'Где ловится',
-        'coordinate-view' => 'Координаты на которых был замечен',
+        'title' => 'List of all fish and their habitats',
+        'name' => 'Name',
+        'weight' => 'Weight',
+        'title-weight' => 'Minimum - maximum fish weight',
+        'where' => 'Where is caught',
+        'coordinate-view' => 'Coordinates where :name was spotted',
     ],
 
     'sidebar' => [
-        'wiki' => 'База знаний по игре',
-        'navigation' => 'Навигация',
+        'wiki' => 'Knowledge base for the game',
+        'navigation' => 'Menu',
     ]
 
 ];
